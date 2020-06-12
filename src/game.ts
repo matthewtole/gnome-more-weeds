5ee1d30d5cc33d784dd9b77c018bf1e72114df1f
Matthew Tole <matthewtole@gmail.com>
2020-06-12 14:37:21 -0700

    ✨ Add basic tileset to create background

--- src/game.ts
+++ src/game.ts
@@ -1,43 +1,46 @@
 import 'phaser';
 
-export default class Demo extends Phaser.Scene {
+export default class PlantGame extends Phaser.Scene {
   constructor() {
-    super('demo');
+    super('plantgame');
   }
 
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
-    this.load.image('libs', 'assets/libs.png');
-    this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-    this.load.glsl('stars', 'assets/starfields.glsl.js');
+    this.load.spritesheet('tiles', 'assets/tiles32.png', {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
   }
 
   create() {
-    this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+    this.add.sprite(16, 16, 'tiles', 89);
+    for (let t = 1; t < 15; t += 1) {
+      this.add.sprite(16 + 32 * t, 16, 'tiles', 90);
+    }
+    this.add.sprite(512 - 16, 16, 'tiles', 91);
 
-    this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
+    this.add.sprite(16, 512 - 16, 'tiles', 135);
+    for (let t = 1; t < 15; t += 1) {
+      this.add.sprite(16 + 32 * t, 512 - 16, 'tiles', 136);
+    }
+    this.add.sprite(512 - 16, 512 - 16, 'tiles', 137);
 
-    this.add.image(400, 300, 'libs');
+    for (let t = 1; t < 15; t += 1) {
+      this.add.sprite(16, 16 + 32 * t, 'tiles', 112);
+    }
 
-    const logo = this.add.image(400, 70, 'logo');
-
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
-      repeat: -1,
-    });
+    for (let t = 1; t < 15; t += 1) {
+      this.add.sprite(512 - 16, 16 + 32 * t, 'tiles', 114);
+    }
   }
 }
 
 const config = {
   type: Phaser.AUTO,
-  backgroundColor: '#125555',
-  width: 800,
-  height: 600,
-  scene: Demo,
+  backgroundColor: '#b06758',
+  width: 512,
+  height: 512,
+  scene: PlantGame,
 };
 
 const game = new Phaser.Game(config);
